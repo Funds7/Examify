b599397347a4b543e27e836978d2fa151b79127b
Funds7 <odigwejoshua7@gmail.com>
2026-06-12 00:50:22 +0100

    Organize questions into courses structure
    Refactored questions into courses object with multiple subjects.

--- questions.js
+++ questions.js
@@ -1,27 +1,51 @@
-const questions = [
-  {
-    question: "What does ICT stand for?",
-    options: [
-      "Information and Communication Technology",
-      "International Communication Technology",
-      "Internet Computer Transfer",
-      "Information Control Theory"
-    ],
-    answer: 0
-  },
-  {
-    question: "Nigeria gained independence in what year?",
-    options: ["1960", "1950", "1970", "1980"],
-    answer: 0
-  },
-  {
-    question: "HTML is used for?",
-    options: [
-      "Styling websites",
-      "Programming AI",
-      "Structuring web pages",
-      "Database management"
-    ],
-    answer: 2
-  }
-];
+const courses = {
+  gst101: [
+    {
+      question: "What does ICT stand for?",
+      options: [
+        "Information and Communication Technology",
+        "International Communication Technology",
+        "Internet Computer Transfer",
+        "Information Control Theory"
+      ],
+      answer: 0
+    },
+
+    {
+      question: "A library is primarily used for?",
+      options: [
+        "Sports",
+        "Reading and Research",
+        "Cooking",
+        "Banking"
+      ],
+      answer: 1
+    }
+  ],
+
+  gst102: [
+    {
+      question: "Who is called the father of philosophy?",
+      options: [
+        "Socrates",
+        "Einstein",
+        "Newton",
+        "Plato"
+      ],
+      answer: 0
+    }
+  ],
+
+  gst103: [
+    {
+      question: "DELSU means?",
+      options: [
+        "Delta State University",
+        "Delta Science University",
+        "Delta School University",
+        "Delta Student Union"
+      ],
+      answer: 0
+    }
+  ]
+};
