fb6f413844637d5ce2c3991e2fdf980c6f3a1a3d
Funds7 <odigwejoshua7@gmail.com>
2026-06-12 23:53:50 +0100

    Add chapters for GST 101 course curriculum

---
+++ gst101.js
@@ -0,0 +1,42 @@
+const gst101Chapters = [
+  {
+    id: 1,
+    title: "An Introduction to English Lexis and Structure"
+  },
+  {
+    id: 2,
+    title: "Essay Writing, Paragraph and Punctuation"
+  },
+  {
+    id: 3,
+    title: "Phonetics"
+  },
+  {
+    id: 4,
+    title: "Reading and Studying Skills"
+  },
+  {
+    id: 5,
+    title: "The Art of Public Speaking and Oral Communication"
+  },
+  {
+    id: 6,
+    title: "Sentence Construction"
+  },
+  {
+    id: 7,
+    title: "The Significance of the Library in University Education"
+  },
+  {
+    id: 8,
+    title: "Reference Services, Sources, Bibliographic Citation and Reference List"
+  },
+  {
+    id: 9,
+    title: "Electronic Library and Copyright"
+  },
+  {
+    id: 10,
+    title: "Library Management, Organization and Retrieval of Materials"
+  }
+];
