908d4879bcf0cdeac35896c77736cff9ef8b7c2e
Funds7 <odigwejoshua7@gmail.com>
2026-06-13 00:41:17 +0100

    Initialize gst102Chapters for philosophy curriculum
    This array contains chapters relevant to the GST102 philosophy course, covering various topics in philosophy.

---
+++ gst102.js
@@ -0,0 +1,42 @@
+const gst102Chapters = [
+  {
+    id: 1,
+    title: "General Introduction to Philosophy"
+  },
+  {
+    id: 2,
+    title: "Periods in the History of Western Philosophy"
+  },
+  {
+    id: 3,
+    title: "The Problems of Philosophy"
+  },
+  {
+    id: 4,
+    title: "Ethics and Some of its Attendant Contemporary Issues"
+  },
+  {
+    id: 5,
+    title: "Introduction to Africa Philosophy"
+  },
+  {
+    id: 6,
+    title: "Philosophy and National Development"
+  },
+  {
+    id: 7,
+    title: "Philosophy, Democracy and Human Rights"
+  },
+  {
+    id: 8,
+    title: "Definition and Scope of Logic"
+  },
+  {
+    id: 9,
+    title: "General Natures of Fallacies"
+  },
+  {
+    id: 10,
+    title: "Categorical Propositions and Inferences"
+  }
+];
