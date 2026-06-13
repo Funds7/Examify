e406546f3898aa45391b5d576b5622b64e05bf38
Funds7 <odigwejoshua7@gmail.com>
2026-06-13 01:33:36 +0100

    Initialize gst111Chapters with course chapter titles
    This file introduces an array containing chapter titles relevant to the GST 111 course, covering various aspects of Nigerian culture, politics, and education.

---
+++ gst111.js
@@ -0,0 +1,42 @@
+const gst111Chapters = [
+  {
+    id: 1,
+    title: "The Significance of National and Institutional Anthems: The case of Nigeria and Delta State University"
+  },
+  {
+    id: 2,
+    title: "Nigerian Peoples' Culture and Trends Since Pre-colonial Times"
+  },
+  {
+    id: 3,
+    title: "Elements of Nigerian Government and Politics"
+  },
+  {
+    id: 4,
+    title: "Musical Arts Education and Opportunities for Entrepreneurship in Nigeria"
+  },
+  {
+    id: 5,
+    title: "Norms, Values, Beliefs and Practices in Nigeria"
+  },
+  {
+    id: 6,
+    title: "The Concept of Evil in Nigeria Indigenous Religions"
+  },
+  {
+    id: 7,
+    title: "Marketing and its Related Features"
+  },
+  {
+    id: 8,
+    title: "Nigeria Art Forms, Evolution and Significance"
+  },
+  {
+    id: 9,
+    title: "Nigeria Physical Environment"
+  },
+  {
+    id: 10,
+    title: "Principles of Diseases Prevention and Control"
+  }
+];
