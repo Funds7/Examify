fc97fa53b7982bfc591fc7814189217e07f230bd
Funds7 <odigwejoshua7@gmail.com>
2026-06-13 02:38:15 +0100

    Define gst114Chapters array with course content
    This file defines an array of chapters for the GST 114 course, each with an ID and title.

---
+++ gst114.js
@@ -0,0 +1,42 @@
+const gst114Chapters = [
+  {
+    id: 1,
+    title: "General Introduction"
+  },
+  {
+    id: 2,
+    title: "The French Alphabets and their Pronunciation"
+  },
+  {
+    id: 3,
+    title: "La Famille"
+  },
+  {
+    id: 4,
+    title: "Direction"
+  },
+  {
+    id: 5,
+    title: "Nationality"
+  },
+  {
+    id: 6,
+    title: "Number/figures and their Usage"
+  },
+  {
+    id: 7,
+    title: "Le Pluriel en Francais/Plurality in French"
+  },
+  {
+    id: 8,
+    title: "Quelle Heure Est-il"
+  },
+  {
+    id: 9,
+    title: "Comment se Presenter A L'Aeroport, A la Douane et a L'Immigration Formalities"
+  },
+  {
+    id: 10,
+    title: "Les Prefessions Professions"
+  }
+];
