e4cc3845990670cae27c731153eddb9064026460
Funds7 <odigwejoshua7@gmail.com>
2026-06-13 01:47:54 +0100

    Define gst112Chapters with chapter information

---
+++ gst112.js
@@ -0,0 +1,42 @@
+const gst112Chapters = [
+  {
+    id: 1,
+    title: "Evolution and the Origin of Man"
+  },
+  {
+    id: 2,
+    title: "Man and His Cosmic Environment"
+  },
+  {
+    id: 3,
+    title: "History of Science and Science Methodology"
+  },
+  {
+    id: 4,
+    title: "Science and Technology in the Services of Man"
+  },
+  {
+    id: 5,
+    title: "Energy Sources: Generation and Uses"
+  },
+  {
+    id: 6,
+    title: "Pollution Problems and Environmental Effects of Chemicals"
+  },
+  {
+    id: 7,
+    title: "Inorganic Chemicals and Minerals"
+  },
+  {
+    id: 8,
+    title: "Living Things: Their Characteristics and Diversity"
+  },
+  {
+    id: 9,
+    title: "Agriculture Policy, Food Question and Agricultural Products in Nigeria"
+  },
+  {
+    id: 10,
+    title: "Pesticides and Their Effects on the Environment"
+  }
+];
