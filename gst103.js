4aec863be436271419ec9a98757f932d61c15c0a
Funds7 <odigwejoshua7@gmail.com>
2026-06-13 01:04:26 +0100

    Add chapter details for GST103 course

---
+++ gst103.js
@@ -0,0 +1,34 @@
+const gst103Chapters = [
+  {
+    id: 1,
+    title: "Delta State Environment"
+  },
+  {
+    id: 2,
+    title: "About Delta State University"
+  },
+  {
+    id: 3,
+    title: "University Service Departments and Their Functions"
+  },
+  {
+    id: 4,
+    title: "Safety and Security Measures on Campus"
+  },
+  {
+    id: 5,
+    title: "Student Unionism and Alumni Association"
+  },
+  {
+    id: 6,
+    title: "Library Management"
+  },
+  {
+    id: 7,
+    title: "Library Services"
+  },
+  {
+    id: 8,
+    title: "Promoting Academic Excellence"
+  }
+];
