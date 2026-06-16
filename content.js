b6429c6f4a5d895742f72306cb032656794e4765
Funds7 <odigwejoshua7@gmail.com>
2026-06-16 03:20:33 +0100

    Define CONTENT object with grammar chapter details

---
+++ content.js
@@ -0,0 +1,17 @@
+const CONTENT = {
+
+  gst101: {
+
+    chapter1: [
+
+      "Lexis and structure are also known as the study of the word and the structure of the sentence constitute the two most basic units of grammar.",
+
+      "For Huddleston, the two most basic units of grammar are the word and the sentence.",
+
+      "The two most basic units of grammar are the word and the sentence."
+
+    ]
+
+  }
+
+};
