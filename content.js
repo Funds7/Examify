5fb752e27d19f1d5afd5d402ce5e98079bbf62e2
Funds7 <odigwejoshua7@gmail.com>
2026-06-18 00:14:06 +0100

    Enhance Chapter 2 with essay writing details
    Expanded Chapter 2 with detailed explanations of essay writing stages, types of essays, and punctuation usage.

--- content.js
+++ content.js
@@ -119,8 +119,93 @@ const CONTENT = {
 "Examples of coordinating conjunctions are and, but and or.",
 
 "A complex sentence consists of one or more main clauses and one or more subordinate clauses."
+chapter1: [
+      // Your Chapter 1 points here
+    ],
 
-]
+    chapter2: [
+
+      "An essay can be defined as any piece of writing on related ideas with each idea overlapping with one another until they form a unified whole.",
+
+      "A text is any piece of writing of any length that has unity.",
+
+      "Material required for essay is language.",
+
+      "The major aim of the essay is to communicate.",
+
+      "To write a good essay, the essayist must be in control of the idea which he wants to communicate to readers.",
+
+      "The types of essays include: 1. Narrative essay 2. Descriptive essay 3. Expository essay 4. Argumentative essay.",
+
+      "The purpose of a narrative essay is to tell a story in chronological order.",
+
+      "For effective narration adhere to the following guidelines 1. Use the past tense form of the verb since the story is of events that occurred in the past.",
+
+      "The purpose of a descriptive essay is to evoke a mood or create a dominant impression about an object, a person, or a place.",
+
+      "To write a good descriptive essay, you have to be a good observer of events and objects to be described.",
+
+      "For effective description, adhere to the following guidelines 1. Be proficient in language 2. Try as much as possible to invoke all the senses 3. Give specific details.",
+
+      "The purpose of an expository essay is to inform, clarify, explain or to expose or tell how something is done.",
+
+      "An argumentative essay is also referred to as a persuasive essay. The purpose is to change the reader's point of view.",
+
+      "To be effective a good argumentative essay should be objective without bias, be based on reasons, verifiable facts, and statistics.",
+
+      "The three stages involved in writing the essay include 1. Pre-writing stages 2. Writing stages and 3. Re-writing stages.",
+
+      "The pre-writing stage consists of several activities which the writer must go through before setting out to write the essay.",
+
+      "Activities in the pre-writing stages include: choosing a topic, forming a point of view, searching for facts, analyzing and arranging the facts, and outlining.",
+
+      "An outline is the graphical presentation of all the information in a work and the relationship between them.",
+
+      "The three types of outlines include topic outline, sentence outline, and paragraph outline.",
+
+      "A good outline should facilitate writing as it helps to discover more ideas and interrelationship between them, give structure and organization to the essay, etc.",
+
+      "The writing stage is the actual writing of the essay.",
+
+      "The re-writing stage in the essay is the last. It involves three closely interrelated processes of revising, editing, and proofreading.",
+
+      "According to McCrimmon, the four requirements of a good paragraph are unity, completeness, order, and coherence.",
+
+      "A good paragraph must discuss one idea only.",
+
+      "The full stop(.) also called the period, denotes the longest pause.",
+
+      "Do not use a full stop after shortened or clipped forms e.g 1st, 12th, lab, gym, laser, etc.",
+
+      "The question mark(?) is used at the end of interrogative clauses also known as a question.",
+
+      "The exclamation mark(!) is used after an emphatic interjection or after a phrase, a clause, or a sentence that expresses surprise or strong emotion.",
+
+      "The comma(,) is the shortest of all the pauses.",
+
+      "The semi-colon(:) denotes a pause midway between the full stop and the comma. It is used to join two clauses which are separate but related in sense.",
+
+      "An example of a semi-colon(;) includes the doctor who came out from the theatre with smiles; obviously it was a successful operation.",
+
+      "The colon(:) is used to denote that something is to follow, it also introduces a list.",
+
+      "The example of the colon includes: The pupils are to bring the following items: pens, pencils, paper, books and calculators.",
+
+      "The dash (-) is used to mark a sudden change of thought. For example, I will go jogging this evening-oh! I am yet to do my assignment.",
+
+      "The capital is used to indicate the beginning of a sentence, as the first letter in proper noun, etc.",
+
+      "The hyphen (-) is used after some prefixes such as ex-, anti- etc.",
+
+      "The apostrophe(') is used to show the contracted form of some words e.g won't(will not), don't(do not).",
+
+      "Quotation marks (\") which can be double or single are always in pairs and can be used to enclose a direct quotation or to indicate the actual words of a speaker or writer.",
+
+      "Ellipsis(...) is a mark of omission. It is used to show that some words have been omitted in a sentence.",
+
+      "Brackets/parenthesis(()) is used to mark off words, phrases commentaries in the flow of our writings."
+
+    ]
 
   }
 
