6fed47f6f738a01911f7f7e0eb3d2739165d1e65
Funds7 <odigwejoshua7@gmail.com>
2026-07-12 04:26:42 +0100

    Add Firebase email/password login functionality
    Implemented login functionality using Firebase authentication to allow users to sign in with their email and password.

---
+++ login.js
@@ -0,0 +1,42 @@
+import { auth } from "./firebase.js";
+
+import {
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+
+const loginBtn = document.getElementById("loginBtn");
+
+
+loginBtn.onclick = async()=>{
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+
+try{
+
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+
+alert("Login successful 🎉");
+
+window.location.href="courses.html";
+
+
+}
+
+catch(error){
+
+alert(error.message);
+
+}
+
+};
